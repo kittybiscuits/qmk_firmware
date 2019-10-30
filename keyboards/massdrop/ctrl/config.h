51292c44fc325cf256c6f9023ff2cf5bfde25902
Kitty Biscuits <kittybiscuits@users.noreply.github.com>
2019-10-30 10:59:18 -0700

    Added "cat on keyboard" layers to Massdrop CTRL.
    Kitty enjoys laying across the top row of keys at times.

--- keyboards/massdrop/ctrl/config.h
+++ keyboards/massdrop/ctrl/config.h
@@ -125,3 +125,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
 
 #include "config_led.h"
+
+/*
+ * This allows us to press Shift+Esc to get a tilde ("~") character. Unfortunately,
+ * this breaks certain key combinations. The following #defines restore those key
+ * combinations while still allowing Shift+Esc to send a tilde character.
+ *
+ * See: https://github.com/qmk/qmk_firmware/blob/master/docs/feature_grave_esc.md
+ *
+ * GRAVE_ESC_ALT_OVERRIDE  - Allows Command+Option+Escape to work on Macs.
+ * GRAVE_ESC_CTRL_OVERRIDE - Allows Control+Shift+Escape to work on Windows.
+ * GRAVE_ESC_GUI_OVERRIDE  - Just in case. :)
+ */
+#define GRAVE_ESC_ALT_OVERRIDE     // Always send Escape if Alt is pressed
+#define GRAVE_ESC_CTRL_OVERRIDE    // Always send Escape if Control is pressed
+#define GRAVE_ESC_GUI_OVERRIDE     // Always send Escape if GUI is pressed
+//#define GRAVE_ESC_SHIFT_OVERRIDE // Always send Escape if Shift is pressed
+
